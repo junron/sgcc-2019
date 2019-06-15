b0f8671fc586b7799340e51b12326fe73aa11b85
Lam Jun Rong <bobo999bobo@outlook.com>
2019-06-15 10:59:17 +0800

    Add pause panel

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseController : MonoBehaviour
+{
+  [SerializeField] private Button continueBtn;
+  [SerializeField] private Button quitBtn;
+
+  private void Start()
+  {
+    this.gameObject.SetActive(false);
+    quitBtn.onClick.AddListener(Application.Quit);
+    continueBtn.onClick.AddListener(UnPause);
+
+  }
+
+  private void UnPause()
+  {
+    Time.timeScale = 1;
+    this.gameObject.SetActive(false);
+  }
+}
