b4c2ad264fe2f41e74e6402a5c12d29a525db2bf
Lam Jun Rong <bobo999bobo@outlook.com>
2019-06-26 09:25:47 +0800

    Add park scene

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+  [SerializeField] private GameObject interactText;
+  [SerializeField] private GameObject target;
+  [SerializeField] private float detectRadius;
+  public Action callback;
+  private bool inRange;
+  private Transform transform1;
+
+  private void Start()
+  {
+    transform1 = target.GetComponent<Transform>();
+  }
+
+  private void FixedUpdate()
+  {
+    float distance = Mathf.Abs(Vector3.Distance(transform1.position, this.transform.position));
+    inRange = distance < detectRadius;
+    interactText.SetActive(inRange);
+    if (!inRange || !Input.GetKeyDown(KeyCode.Space)) return;
+    callback();
+    Destroy(this);
+  }
+}
---
+++ Assets/Scripts/MainMenu/TreeHider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeHider : MonoBehaviour
+{
+  [SerializeField] private GameObject tree, noTree;
+  private bool hasTree = true;
+
+  private void Start()
+  {
+    GetComponent<Button>().onClick.AddListener(() =>
+    {
+      hasTree ^= true;
+      tree.SetActive(hasTree);
+      noTree.SetActive(!hasTree);
+    });
+  }
+}
