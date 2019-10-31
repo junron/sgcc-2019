54034640f8c3c6bd1e517b3246dbd766a038ecb7
Lam Jun Rong <bobo999bobo@outlook.com>
2019-10-31 21:25:38 +0800

    More hazard dialog

--- Assets/Scripts/Falls/Hazard.cs
+++ Assets/Scripts/Falls/Hazard.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
@@ -9,11 +10,17 @@ namespace Falls
   {
     public bool disabled;
     public String name, description;
+    public Sprite correctAnswer;
+    public Sprite[] wrongAnswers;
     private static GameObject hazardPanel;
     private static Text titleText;
     private static Image hazardImage;
     private static Text descriptionText;
     private static Button nextBtn;
+
+    private static GameObject hazardMitigationPanel;
+    private static Text titleText2;
+    private static Image hazardImage2;
 
     public void Start()
     {
@@ -28,10 +35,32 @@ namespace Falls
       Assert.IsNotNull(descriptionText);
       nextBtn = hazardPanel.transform.Find("NextBtn").GetComponent<Button>();
       Assert.IsNotNull(nextBtn);
+      hazardMitigationPanel = GameObject.Find("HazardMitigation");
+      Assert.IsNotNull(hazardMitigationPanel);
+      titleText2 = hazardMitigationPanel.transform.Find("TitleText").GetComponent<Text>();
+      Assert.IsNotNull(titleText2);
+      hazardImage2 = hazardMitigationPanel.transform.Find("HazardImage").GetComponent<Image>();
+      Assert.IsNotNull(hazardImage2);
       hazardPanel.transform.parent.gameObject.SetActive(false);
+      hazardMitigationPanel.gameObject.SetActive(false);
       nextBtn.onClick.AddListener(() =>
       {
-        print("CLicked");
+        hazardMitigationPanel.gameObject.SetActive(true);
+        hazardPanel.gameObject.SetActive(false);
+
+        hazardImage2.sprite = GetComponent<SpriteRenderer>().sprite;
+        titleText2.text = "Hazard: " + name;
+        // Shuffle answers
+        Sprite[] allImages = new[] {correctAnswer}.Concat(wrongAnswers).ToArray().OrderBy(a => Guid.NewGuid()).ToArray();
+        int i = 0;
+        foreach (Button button in hazardMitigationPanel.transform.GetComponentsInChildren<Button>())
+        {
+          int i1 = i;
+          print(button.transform.GetComponentInChildren<Image>());
+          button.transform.GetComponentInChildren<Image>().sprite = allImages[i];
+          button.onClick.AddListener(() => { print(allImages[i1]); });
+          i++;
+        }
       });
     }
 
