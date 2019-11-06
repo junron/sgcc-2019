f3e8d896906fd39af526dbea52b6f72bf0c1d845
Lam Jun Rong <bobo999bobo@outlook.com>
2019-11-06 18:53:05 +0800

    Fix ray

--- Assets/Scripts/Game/Card.cs
+++ Assets/Scripts/Game/Card.cs
@@ -4,69 +4,101 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    public int type;
-    Rigidbody2D rb2d;
-    private static int CARD_INDEX = 0;
-    private static float xK = 0.1f, yK= 0.1f; //stands for x constant and y constant 
-    private float[] x = {xK * -45, xK * -15, xK * 15, xK * 45}, y = {yK * -45, yK * -15, yK * 15, yK * 45}; 
-    private int index;
-    public bool deactivated = false;
-    private int chosenSlot = -1;
-    
-    void Awake(){
-        rb2d = GetComponent<Rigidbody2D>();
-        Vector2 position = rb2d.position;
-        position.Set(x[CARD_INDEX], y[CARD_INDEX]);
-        this.type = CARD_INDEX/2;
-        SetColor();
-        index = CARD_INDEX;
-        CARD_INDEX++;
+  public int type;
+  private static int CARD_INDEX;
+  private static float xK = 0.8f, yK = 0.1f; //stands for x constant and y constant
+  private float[] x = {xK * -45, xK * -15, xK * 15, xK * 45}, y = {yK * -45, yK * -15, yK * 15, yK * 45};
+  private int index;
+  public bool deactivated;
+  private int chosenSlot = -1;
+
+  private static int[] types = {0, 0, 1, 1, 2, 2, 3, 3, -1};
+
+  private void SetType()
+  {
+    int temp = 8;
+    while (types[temp] == -1){
+      temp = Random.Range(0,8);
+    }
+    type = types[temp];
+    types[temp] = -1;
+  }
+  void Awake()
+  {
+    this.transform.position = new Vector2(x[CARD_INDEX % 4], y[CARD_INDEX / 4]);
+    SetType();
+    SetColor();
+    index = CARD_INDEX;
+    CARD_INDEX++;
+  }
+
+  void OnMouseOver()
+  {
+    if (deactivated)
+    {
+      return;
     }
 
-    void Update(){
-        
-    }
-    void OnMouseOver(){
-        if (deactivated){return;}
-        if(Input.GetMouseButtonDown(0)){
-            if (chosenSlot != -1){//if already chosen
-                MemoryGame.cardsChosen[chosenSlot] = -1; //remove from chosen slot
-                flip();
-            }
-            else if (chosenSlot == -1){ //if not chosen
-                if (MemoryGame.cardsChosen[0] == -1){ //if slot0 not chosen
-                    MemoryGame.cardsChosen[0] = index;
-                    chosenSlot = 0;
-                }
-                else if (MemoryGame.cardsChosen[1] != -1){ // if slot1 not chosen
-                    MemoryGame.cardsChosen[1] = index;
-                    chosenSlot = 1;
-                }
-                flip();
-            }
-            
+    if (Input.GetMouseButtonDown(0))
+    {
+      if (chosenSlot != -1)
+      {
+        //if already chosen
+        MemoryGame.cardsChosen[chosenSlot] = -1; //remove from chosen slot
+        flip();
+      }
+      else if (chosenSlot == -1)
+      {
+        //if not chosen
+        if (MemoryGame.cardsChosen[0] == -1)
+        {
+          //if slot0 not chosen
+          MemoryGame.cardsChosen[0] = index;
+          chosenSlot = 0;
         }
-    }
-
-
-    
-    public void flip(){
-        if (chosenSlot != -1){
-            chosenSlot = -1;
-            GetComponent<SpriteRenderer>().color = Color.black;
+        else if (MemoryGame.cardsChosen[1] != -1)
+        {
+          // if slot1 not chosen
+          MemoryGame.cardsChosen[1] = index;
+          chosenSlot = 1;
         }
 
-        else if (chosenSlot == -1){
-            SetColor();
-        }
+        flip();
+      }
+    }
+  }
+
+
+  public void flip()
+  {
+    if (chosenSlot != -1)
+    {
+      chosenSlot = -1;
+      GetComponent<SpriteRenderer>().color = Color.black;
     }
 
-    private void SetColor(){
-        switch(type){
-                case 0: GetComponent<SpriteRenderer>().color = Color.white;break;
-                case 1: GetComponent<SpriteRenderer>().color = Color.red;break;
-                case 2: GetComponent<SpriteRenderer>().color = Color.green;break;
-                case 3: GetComponent<SpriteRenderer>().color = Color.blue;break;
-            }
+    else if (chosenSlot == -1)
+    {
+      SetColor();
     }
+  }
+
+  private void SetColor()
+  {
+    switch (type)
+    {
+      case 0:
+        GetComponent<SpriteRenderer>().color = Color.white;
+        break;
+      case 1:
+        GetComponent<SpriteRenderer>().color = Color.red;
+        break;
+      case 2:
+        GetComponent<SpriteRenderer>().color = Color.green;
+        break;
+      case 3:
+        GetComponent<SpriteRenderer>().color = Color.blue;
+        break;
+    }
+  }
 }
--- Assets/Scripts/Game/MemoryGame.cs
+++ Assets/Scripts/Game/MemoryGame.cs
@@ -1,50 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class MemoryGame : MonoBehaviour
-{   
-    [SerializeField] private GameObject cardPrefab;    
-    private GameObject[] cardlst = new GameObject[8];
-    public static int[] cardsChosen = new int[2];
-    
-    
+{
+  [SerializeField] private GameObject cardPrefab;
+  private static int cardNo = 8;
+  private Card[] cards = new Card[cardNo];
+  public static int[] cardsChosen = new int[2];
 
-    // Start is called before the first frame update
-    void Start()
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    InitializeCards();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (cardsChosen[0] != -1 && cardsChosen[1] != -1)
     {
-        cardlst = new GameObject[8];
-        InitializeCards();
+      Check(); //if 2 cards chosen
     }
+  }
 
-    // Update is called once per frame
-    void Update()
+  //color code and pair the cards
+  private void InitializeCards()
+  {
+    for (int i = 0; i < cardNo; i++)
     {
-        if (cardsChosen[0] != -1 && cardsChosen[1] != -1){
-                    check(); //if 2 cards chosen
-                }
+      GameObject card = Instantiate(cardPrefab);
+      cards[i] = card.GetComponent<Card>();
+      Assert.IsNotNull(cards[i]);
+    }
+  }
+
+  private void Check()
+  {
+    if (cardsChosen[0] == -1 || cardsChosen[1] == -1)
+    {
+      return;
     }
 
-    //color code and pair the cards
-    private void InitializeCards(){
-        for (int i = 0; i < 8; i ++){
-            
-            GameObject card = Instantiate(cardPrefab);
-            cardlst[i] = card;
-        }
+    Card card1 = cards[cardsChosen[0]], card2 = cards[cardsChosen[1]];
+    if (card1.type == card2.type)
+    {
+      //correct!
+      card1.deactivated = true;
+      card1.gameObject.SetActive(false);
     }
-
-    public void check(){
-        if (cardsChosen[0] == -1 ||cardsChosen[1] == -1){return;}
-//        Card card1 = cardlst[cardsChosen[0]], card2 =cardlst[cardsChosen[1]];
-//        if(card1.type == card2.type){
-           //correct!
-//            card1.deactivated = true;
-//            card1.card.SetActive(false);
-//        }
-//        else{
-//            cardsChosen[0] = -1; cardsChosen[1] = -1;
-//            card1.flip(); card2.flip();
-//        }
+    else
+    {
+      cardsChosen[0] = -1;
+      cardsChosen[1] = -1;
+      card1.flip();
+      card2.flip();
     }
+  }
 }
